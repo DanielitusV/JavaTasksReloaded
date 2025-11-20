23869b10222491907a3f9c11eeabbf0d0b4da502
Daniel_Virreira <virreira.daniel@gmail.com>
2025-11-19 22:54:10 -0400

    Agregar funcionalidad para trabajar con tareas directamente en memoria

---
+++ src/persistence/InMemoryTaskRepository.java
@@ -0,0 +1,32 @@
+package persistence;
+import domain.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Implementación de TaskRepository en memoria.
+ * No usar archivos ni bae de datos, solo mantiene las tareas en una lista.
+ */
+public class InMemoryTaskRepository implements TaskRepository {
+    private final List<Task> tasks = new ArrayList<>();
+
+    /*
+     * Devuelve una copia de la lista de tareas en memoria.
+     */
+    @Override
+    public List<Task> loadAll() {
+        return new ArrayList<>(tasks);
+    }
+
+    /*
+     * Reemplaza el contenido interno por la lista de tareas recibida.
+     */
+    @Override
+    public void saveAll(List<Task> tasks) {
+        this.tasks.clear();
+        if (tasks != null) {
+            this.tasks.addAll(tasks);
+        }
+    }
+}
